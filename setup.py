c1fcba2770b6a987fe7e893954fc72951b938752
baptiste-urgell <baptiste.urgell@aqsone.com>
2024-04-03 20:58:36 +0000

    Add EDA and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stranger-section-2',
+    version='1.0',
+    author='RosIA',
+    author_email='rosialab31@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        # listez vos dépendances ici
+    ],
+)
